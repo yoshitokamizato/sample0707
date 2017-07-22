72927ba63e85f8be9bd17cca55bd8d74f4b863f6
Yoshito Kamizato <yuyueureka157@gmail.com>
2017-07-22 20:47:18 +0900

    つぶやき機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'top' => 'top#index'
-  get 'design' => 'design#index'
-  get 'engineer' => 'engineer#index'
-  get 'programming' => 'programming#index'
+  get 'tweets/top' => 'top#index'
+  get 'tweets/design' => 'design#index'
+  get 'tweets/engineer' => 'engineer#index'
+  get 'tweets/programming' => 'programming#index'
+  get 'tweets/new' => 'tweets#new'
 end
